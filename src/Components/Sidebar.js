3aeda1a4e622f8b5edf2dffec42cc50810a758a7
Racso01 <oscarmbarter@gmail.com>
2020-03-14 18:50:53 +0000

    edit for new functuionality and deployment

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import image from '../Assets/profile.jpg';
+//import image from './src/Assets/profile.jpg';
 
 class Sidebar extends Component {
     constructor(props) {
@@ -14,7 +14,7 @@ class Sidebar extends Component {
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                     <span className="d-block d-lg-none">{this.sidebarData.firstName} {this.sidebarData.lastName}</span>
                     <span className="d-none d-lg-block">
-                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={image} alt=""></img>
+                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={"http://</span>i0.wp.com/www.spottedinely.com/wp-content/uploads/2019/11/Oscar-Barter-Cambridgeshire-District-Cadet-of-the-Year-2020-scaled-e1574792828749.jpg?fit=1024%2C644&ssl=1" } alt=""></img>
                     </span>
                 </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
